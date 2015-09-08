3b81dad2fbf0843b2d78b677400303afe1aeeab5
Jake W <jakewiseberg@gmail.com>
2015-09-08 19:26:08 -0400

    Ew Homework 9/8

---
+++ Work/CompSci/src/TextookWork9815/E1Point1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TextookWork9815;
+
+/**
+ *
+ * @author jakew
+ */
+public class E1Point1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Universe!!");
+    }
+}
---
+++ Work/CompSci/src/TextookWork9815/E1Point11.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TextookWork9815;
+
+/**
+ *
+ * @author jakew
+ */
+public class E1Point11 {
+    public static void main(String[] args) {
+        System.out.println("My Three Favorite Movies");
+        System.out.println("1. Inception");
+        System.out.println("2. Fight Club");
+        System.out.println("3. Interstellar");
+    }
+}
