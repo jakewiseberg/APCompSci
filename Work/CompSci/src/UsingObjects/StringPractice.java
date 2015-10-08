a7fd30b3edadcbc3df00b071f5b5b27576617b4b
unknown <jacob.wiseberg@LHS-LN-A217-18.livingston.org>
2015-10-08 13:28:38 -0400

    the first lab!

--- Work/CompSci/src/UsingObjects/StringPractice.java
+++ Work/CompSci/src/UsingObjects/StringPractice.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package UsingObjects;
+
+import java.util.Scanner;
+
 /**
  *
  * @author jacob.wiseberg
@@ -11,7 +14,7 @@ package UsingObjects;
 public class StringPractice {
     public static void main(String[] args) {
         //String word = new String("The word"); --> can be used but functions the same as below
-        String food = "sweet potato pie";
+        /*String food = "sweet potato pie";
         System.out.println(food.length());
         System.out.println(food.charAt(5));
         System.out.println(food.substring(8));
@@ -19,5 +22,16 @@ public class StringPractice {
         System.out.println(food.substring(0,5).length());
         System.out.println(food.indexOf("e"));
         System.out.println(food.compareTo("sweaty potato pie"));
+        */
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Input word: ");
+        String word = reader.nextLine();
+        System.out.println("Last letter of word: " + lastLetter(word));
+        System.out.println(word.equals("idk"));
+        System.out.println(word.equalsIgnoreCase("idk"));
+    }
+    
+    public static char lastLetter(String word) {
+        return word.charAt(word.length()-1);
     }
 }
