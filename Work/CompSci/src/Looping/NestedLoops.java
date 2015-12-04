de7d5cef7fad056291775f4677a0456f8e546368
unknown <jacob.wiseberg@LHS-LN-A217-18.livingston.org>
2015-12-04 13:39:29 -0500

    prime composite unique program

--- Work/CompSci/src/Looping/NestedLoops.java
+++ Work/CompSci/src/Looping/NestedLoops.java
@@ -6,19 +6,21 @@ package Looping;
  */
 public class NestedLoops {
     public static void main(String[] args) {
-        xBox(5);
-        System.out.println("");
-        xRightTriangle(5);
-        System.out.println("");
-        xIsoTriangle(5);
-        System.out.println("");
-        numBox(3);
-        System.out.println("");
-        numPyramid(5);
-        System.out.println("");
-        numTriangle(5);
-        System.out.println("");
-        powBox(3);
+        xIsoTriangle(3);
+        
+//        xBox(5);
+//        System.out.println("");
+//        xRightTriangle(5);
+//        System.out.println("");
+//        xIsoTriangle(3);
+//        System.out.println("");
+//        numBox(3);
+//        System.out.println("");
+//        numPyramid(5);
+//        System.out.println("");
+//        numTriangle(5);
+//        System.out.println("");
+//        powBox(3);
     }
     
     public static void xBox(int n) {
@@ -38,14 +40,20 @@ public class NestedLoops {
     }
     
     public static void xIsoTriangle(int n) {
-        for (int r=1; r<=n; r++) {
-            for (int c=1; c<=r; c++)
-                System.out.print("x");
-            System.out.println("");
-        }
-        for (int r=n; r>0; r--) {
-            for (int c=r; c>0; c--)
-                System.out.print("x");
+//        for (int r=1; r<=n; r++) {
+//            for (int c=1; c<=r; c++)
+//                System.out.print("x");
+//            System.out.println("");
+//        }
+//        for (int r=n; r>0; r--) {
+//            for (int c=r; c>0; c--)
+//                System.out.print("x");
+//            System.out.println("");
+//        }
+        
+        for(int i=1; i<=2*n-1; i++) {
+            for (int j=1; j<n-Math.abs(n-i); j++)
+                System.out.println("x");
             System.out.println("");
         }
     }
