07e8c58e4aa56d713132bdc6ed3d5abf997169e3
Jake Wiseberg <Jake Wiseberg>
2016-01-19 18:24:51 -0500

    finished min and max problems for ArrayLists

--- Work/CompSci/src/ArrayLists/PrimitiveObjects.java
+++ Work/CompSci/src/ArrayLists/PrimitiveObjects.java
@@ -7,7 +7,10 @@ import java.util.*;
  */
 public class PrimitiveObjects {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = generate(20);
+        System.out.println("List: " + list);
+        System.out.println("Highest number: " + max(list));
+        System.out.println("Lowest number: " + min(list));
                 
     }
     
@@ -37,7 +40,24 @@ public class PrimitiveObjects {
         }
         System.out.println(list);
     }
-    public static void max(ArrayList<Integer> list) {
-        
+    public static int max(ArrayList<Integer> list) {
+        int max = list.get(0);
+        for (int i=1; i<list.size(); i++)
+            if (list.get(i)>max) max = list.get(i);
+        return max;
+    }
+    
+    public static int min(ArrayList<Integer> list) {
+        int min = list.get(0);
+        for (int i=1; i<list.size(); i++)
+            if (list.get(i)<min) min = list.get(i);
+        return min;
+    }
+    
+    public static ArrayList<Integer> generate(int size) {
+        ArrayList<Integer> list = new ArrayList();
+        for (int i=0; i<size; i++)
+            list.add((int)(Math.random()*20+1));
+        return list;
     }
 }
