ad33c1257abf4d79a7026d99deb4a1e1f8477a78
unknown <jacob.wiseberg@LHS-LN-A217-18.livingston.org>
2016-02-17 12:37:49 -0500

    finished all buuut insertion, needs fixing

--- Work/CompSci/src/SortingSearching/Sorting.java
+++ Work/CompSci/src/SortingSearching/Sorting.java
@@ -6,10 +6,11 @@ package SortingSearching;
  */
 public class Sorting {
     public static void main(String[] args) {
-        int[] test = {1,5,2,7,9,3,8,25,9,3};
-        System.out.println("Unsorted  : " + toString(test));
-        insertion(test);
-        System.out.println("Sorted    : " + toString(test));
+        int[] a = {9,8,7,6,5,4,3,2,1,0};
+        System.out.println("Unsorted   : " + toString(a));
+        insertion(a);
+        System.out.println("Sorted     : " + toString(a));
+        
     }
     
     public static void bubble(int[] a) {
@@ -17,6 +18,7 @@ public class Sorting {
             for (int j=0; j<i; j++)
                 if (a[j] > a[j+1])
                     swap(a, j, j+1);
+        
     }
     
     public static void selection(int[] a) {
@@ -38,8 +40,27 @@ public class Sorting {
             while (j>0 && a[j]<a[j-1]) {
                 swap(a, a[j], a[j-1]);
                 j--;
+                System.out.println(toString(a));
             }
+            System.out.println("Finished iteration " + i + " --->   " + toString(a));
         }
+    }
+    
+    //precondition: the arrays are sorted
+    public static void merge(int[] a, int[] b) {
+        int i=0, j=0, k=0;
+        int[] merged = new int[a.length + b.length - 1];
+        while (i<a.length && j<b.length) {
+            if (a[i] <= b[j]) {
+                merged[k] = a[i];
+                i++;
+            } else {
+                merged[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+        System.out.println(toString(merged));
     }
     
     public static void swap(int[] a, int i1, int i2) {
@@ -49,9 +70,10 @@ public class Sorting {
     }
     
     public static String toString(int[] a) {
-        String str = "";
+        String str = "[";
         for (int i : a)
-            str += (i + " ");
+            str += (" " + i);
+        str += " ]";
         return str;
     }
 }
