78a3d1a8dfaebe012de0ceb54486e44a1c79580d
Jake Wiseberg <jakewiseberg@gmail.com>
2016-05-13 14:08:06 -0400

    quick sort added

--- Work/CompSci/src/SortingSearching/QuickSort.java
+++ Work/CompSci/src/SortingSearching/QuickSort.java
@@ -5,49 +5,66 @@
  */
 package SortingSearching;
 
-import java.util.Arrays;
-
-/**
- *
- * @author jakew
- */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {1,4,2,3,7,5,8,6,9,0};
-        sort(arr);
-        System.out.println(toString(arr));
+        int[] arr = genArray();
+        System.out.println("UNSORTED:  " + printArray(arr));
+        quickSort(arr, 0, arr.length-1);
+        System.out.println("SORTED:    " + printArray(arr));
     }
-    
-    public static int[] sort(int[] arr) {
-        int pivot, wall, curr;
-        for (int i=0; i<arr.length-2; i++) {
-            pivot = arr[arr.length-1];
-            wall = i+1;
-            for (int j=i; j<arr.length-2; j++) {
-                curr = j;
-                if (arr[curr] < pivot) {
-                    swap(arr, curr, wall-1);
-                    wall++;
-                }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (arr == null || arr.length == 0)
+            return;
+        if (low >= high)
+            return;
+
+        // pick the pivot
+        int pivot = arr[(low + (high - low) / 2)];
+
+        // make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+            while (arr[i] < pivot) { i++; }
+            while (arr[j] > pivot) { j--; }
+            if (i <= j) {
+                swap(i, j, arr);
+                i++;
+                j--;
             }
-            swap(arr, wall, arr.length-1);
         }
-        
-        
+
+        // recursively sort two sub parts
+        if (low < j)
+            quickSort(arr, low, j);
+
+        if (high > i)
+            quickSort(arr, i, high);
+    }
+
+
+    public static void swap(int index1,int index2, int[] a){
+        int temp = a[index1];
+        a[index1] = a[index2];
+        a[index2] = temp;
+    }
+
+    public static String printArray(int[] a){
+        String toReturn = "[ ";
+        for(int i : a){
+            toReturn += i + " ";
+        }
+        toReturn += "]";
+        return toReturn;
+    }
+
+    public static int[] genArray(){
+        int[] arr = new int[10];
+        int item = 0;
+        for(int i=0;i<arr.length;i++){
+            item = (int)(Math.random()*100); 
+            arr[i] = item;
+        }
         return arr;
-    }
-    
-    public static void swap(int[] a, int i1, int i2) {
-        int temp = a[i1];
-        a[i1] = a[i2];
-        a[i2] = temp;
-    }
-    
-    public static String toString(int[] a) {
-        String str = "[";
-        for (int i : a)
-            str += (" " + i);
-        str += " ]";
-        return str;
     }
 }
